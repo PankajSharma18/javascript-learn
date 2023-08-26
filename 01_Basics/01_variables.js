300b4ef7c72f0a90d838a39993da04bc753e50c6
PankajSharma18 <90058144+PankajSharma18@users.noreply.github.com>
2023-08-26 06:40:45 +0000

    datatypes done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+var accountId= 1;
+var accountId= 2;
+
+{
+    let accountName= "Sharma";
+}
+
+accountName = "Pankaj Sharma"
+
+var accountName;
+
+console.table([accountId, accountName]);
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,10 @@
+"use strict";
+
+//number
+//bigint
+//string
+//boolean
+//null or undefined
+
+
+console.table([typeof 1, typeof "pankaj", typeof null, typeof undefined]);
